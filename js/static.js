71b7b6232a8ed300c9c9a579177badc9faef9e01
BrightCat14 <geokyll.14@gmail.com>
2025-10-11 21:02:47 +0300

    я тебя ненавижу

--- js/static.js
+++ js/static.js
@@ -1,18 +1,4 @@
 const teamData = [
-    { name: "debianrose", roleKey: "team.role.founder", country: "ru" },
-{ name: "ImIvMih", roleKey: "team.role.designer", country: "ru" },
-{ name: "bodicbilder", roleKey: "team.role.pr", country: "be" },
-{ name: "Baursak4", roleKey: "team.role.smm", country: "ru" },
-{ name: "AkaruiNeko", roleKey: "team.role.coder", country: "ru" }
-];
-
-const featuresData = [
-    { icon: "privacy.svg", titleKey: "features.privacy.title", textKey: "features.privacy.text" },
-{ icon: "server.svg", titleKey: "features.host.title", textKey: "features.host.text" },
-{ icon: "pc.svg", titleKey: "features.pc.title", textKey: "features.pc.text" },
-{ icon: "android.svg", titleKey: "features.android.title", textKey: "features.android.text" }
-];
-const teamData = [
     { 
         name: "debianrose", 
         roleKey: "team.role.founder", 
