1262f2d69e32ed3b5a37a49e9b179660e95c56ea
bodicbilder <phantomfnafer@gmail.com>
2025-10-11 20:46:27 +0300

    Update static.js
    хз че это

--- js/static.js
+++ js/static.js
@@ -12,3 +12,57 @@ const featuresData = [
 { icon: "pc.svg", titleKey: "features.pc.title", textKey: "features.pc.text" },
 { icon: "android.svg", titleKey: "features.android.title", textKey: "features.android.text" }
 ];
+const teamData = [
+    { 
+        name: "debianrose", 
+        roleKey: "team.role.founder", 
+        country: "ru",
+        socials: [
+            { type: "telegram", url: "https://t.me/debianrose" },
+            { type: "github", url: "https://github.com/debianrose" }
+        ]
+    },
+    { 
+        name: "ImIvMih", 
+        roleKey: "team.role.designer", 
+        country: "ru",
+        socials: [
+            { type: "telegram", url: "https://t.me/ImIvMih" },
+            { type: "github", url: "https://github.com/ImIvMih" }
+        ]
+    },
+    { 
+        name: "bodicbilder", 
+        roleKey: "team.role.pr", 
+        country: "be",
+        socials: [
+            { type: "telegram", url: "https://t.me/bodicbilder" },
+            { type: "github", url: "https://github.com/bodicbilder" }
+        ]
+    },
+    { 
+        name: "Baursak4", 
+        roleKey: "team.role.smm", 
+        country: "ru",
+        socials: [
+            { type: "telegram", url: "https://t.me/Baursak4" },
+            { type: "github", url: "https://github.com/Baursak4" }
+        ]
+    },
+    { 
+        name: "AkaruiNeko", 
+        roleKey: "team.role.coder", 
+        country: "ru",
+        socials: [
+            { type: "telegram", url: "https://t.me/AkaruiNeko" },
+            { type: "github", url: "https://github.com/AkaruiNeko" }
+        ]
+    }
+];
+
+const featuresData = [
+    { icon: "privacy.svg", titleKey: "features.privacy.title", textKey: "features.privacy.text" },
+    { icon: "server.svg", titleKey: "features.host.title", textKey: "features.host.text" },
+    { icon: "pc.svg", titleKey: "features.pc.title", textKey: "features.pc.text" },
+    { icon: "android.svg", titleKey: "features.android.title", textKey: "features.android.text" }
+];
